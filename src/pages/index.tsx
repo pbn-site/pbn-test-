b0b7610ffd2687f6a37917b271ecf98a220a3a8f
nhung <nhung@nhungs-MacBook-Air.local>
2022-06-17 16:55:45 +0700

    theme-worksheet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,13 @@
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
 import React from "react";
-import ImageComponent from "../components/common/Image";
-import ContentHome from "../components/content-home";
-import ArrowDownIcon from "../components/Icon/ArrowDown";
-import Layout from "../components/layout";
 import { IPost } from "../models/Post";
 import ThemeMedical from "../theme-medical";
-import { convertToJSONObject, getFullPathAsset } from "../utils";
+import ThemeWorkSheet from "../theme-worksheet";
+import { convertToJSONObject } from "../utils";
 import { readAllPost } from "../utils/FileUtils";
-import styles from "./Index.module.scss";
-import ThemeMedicalSingle from "../theme-medical/single-page";
 import { isWebWorksheet } from "../utils/web";
 const Page = ({ posts }: { posts: IPost[] }) => {
     if (isWebWorksheet()) {
-        return (
-            <Layout title={""} description={""}>
-                <div className={styles.homepage}>
-                    <Container maxWidth="lg">
-                        <div className={styles.homepageContainer}>
-                            <div className={styles.left}>
-                                <div className={styles.title}>Worksheet Zone Blog</div>
-                                <div className={styles.text}>
-                                    Everything you Need to Know to Get Better on your Learning
-                                    Progress
-                                </div>
-                            </div>
-                            <div className={styles.right}>
-                                <ImageComponent
-                                    src={getFullPathAsset("/images/banner-home.png")}
-                                    alt={"banner"}
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <ContentHome posts={posts} />
-                        </div>
-                        <div className={styles.btnSeemore}>
-                            <Button>
-                                See more
-                                <ArrowDownIcon />
-                            </Button>
-                        </div>
-                    </Container>
-                </div>
-            </Layout>
-        );
+        return <ThemeWorkSheet posts={posts} />;
     }
     return <ThemeMedical posts={posts} />;
 };
---
+++ src/theme-worksheet/index.tsx
@@ -0,0 +1,46 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import React from "react";
+import ImageComponent from "../components/common/Image";
+import ContentHome from "../components/content-home";
+import ArrowDownIcon from "../components/Icon/ArrowDown";
+import Layout from "../components/layout";
+import { IPost } from "../models/Post";
+import { getFullPathAsset } from "../utils";
+import styles from "./Index.module.scss";
+
+const ThemeWorkSheet = ({ posts }: { posts: IPost[] }) => {
+    return (
+        <Layout title={""} description={""}>
+            <div className={styles.homepage}>
+                <Container maxWidth="lg">
+                    <div className={styles.homepageContainer}>
+                        <div className={styles.left}>
+                            <div className={styles.title}>Worksheet Zone Blog</div>
+                            <div className={styles.text}>
+                                Everything you Need to Know to Get Better on your Learning Progress
+                            </div>
+                        </div>
+                        <div className={styles.right}>
+                            <ImageComponent
+                                src={getFullPathAsset("/images/banner-home.png")}
+                                alt={"banner"}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <ContentHome posts={posts} />
+                    </div>
+                    <div className={styles.btnSeemore}>
+                        <Button>
+                            See more
+                            <ArrowDownIcon />
+                        </Button>
+                    </div>
+                </Container>
+            </div>
+        </Layout>
+    );
+};
+
+export default ThemeWorkSheet;
