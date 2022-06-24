ffb6edaf71a218e8496936fe28df6af09234bff7
ManhHoangbk <manhhoangbk2@gmail.com>
2022-06-24 11:46:31 +0700

    checkSEO

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,29 @@
 import Container from "@mui/material/Container";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import SEO from "../../theme-medical/seo";
 
 import ImageComponent from "../common/Image";
 import styles from "./Header.module.scss";
 
-const Header = () => {
+interface Props {
+    title: string;
+    description?: string;
+    keywords?: string;
+}
+
+const Header = ({ title, description, keywords = "" }: Props) => {
     const isMobile = useMediaQuery("(max-width: 768px)");
     const srcLogo = isMobile ? "/images/logo-mobile.png" : "/images/logo.png";
 
     return (
-        <Container className={styles.container}>
-            <a href="/">
-                <ImageComponent src={srcLogo} alt={"logo"} />
-            </a>
-        </Container>
+        <>
+            <SEO title={title} description={description} keywords={""} />
+            <Container className={styles.container}>
+                <a href="/">
+                    <ImageComponent src={srcLogo} alt={"logo"} />
+                </a>
+            </Container>
+        </>
     );
 };
 export default Header;
