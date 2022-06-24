87c72369344a419f9530cf3f6c094543893715b9
ManhHoangbk <manhhoangbk2@gmail.com>
2022-06-24 11:50:54 +0700

    createFIleIfNotExit

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 # cd /home/code/work-sheet-zone-blog
 DOMAIN=$1
 FORCE_NEW_DEPLOY=$2
+DIR_BLOG='src/data'
+mkdir $DIR_BLOG
 echo  $DOMAIN
 node node/index.js $DOMAIN $FORCE_NEW_DEPLOY
-DIR_BLOG='src/data'
+
 if [ -d "$DIR_BLOG" ]; then
     totalFile=$(find "$DIR_BLOG" -maxdepth 1 -type f|wc -l)
     echo "Total file: $totalFile"
