46f2bd2893e109e40081e924171166d545b4db4e
nhung <nhungdoca3k41@gmail.com>
2022-06-28 20:34:33 +0700

    fixLogo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import Container from "@mui/material/Container";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import SEO from "../../theme-medical/seo";
+import { isWebWorksheet } from "../../utils/web";
 
 import ImageComponent from "../common/Image";
 import styles from "./Header.module.scss";
@@ -12,25 +13,15 @@ interface Props {
     usingFont?: boolean;
 }
 
-const Header = ({
-    title,
-    description,
-    keywords = "",
-    usingFont = false,
-}: Props) => {
+const Header = ({ title, description, keywords = "", usingFont = false }: Props) => {
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const srcLogo = isMobile
-        ? "/images/logo-mobile.png"
-        : "images/logo-main.webp";
-
+    let srcLogo = isMobile ? "/images/logo-mobile.png" : "images/logo-main.webp";
+    if (isWebWorksheet()) {
+        srcLogo = isMobile ? "/blog/images3/logo-mobile.png" : "/blog/images3/logo.png";
+    }
     return (
         <>
-            <SEO
-                title={title}
-                description={description}
-                keywords={""}
-                usingFont={usingFont}
-            />
+            <SEO title={title} description={description} keywords={""} usingFont={usingFont} />
             <Container className={styles.container}>
                 <a href="/">
                     <ImageComponent src={srcLogo} alt={"logo"} />
