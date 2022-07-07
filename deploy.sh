9324ac0dee646ee4381ec96cbefd8cdad063fa1e
ManhHoangbk <manhhoangbk2@gmail.com>
2022-07-07 14:25:31 +0700

    fixParagraph

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # cd /home/code/work-sheet-zone-blog
+ENV='.env.local'
 DOMAIN=$1
 FORCE_NEW_DEPLOY=$2
 DIR_BLOG='src/data'
 mkdir $DIR_BLOG
 echo  $DOMAIN
 node node/index.js $DOMAIN $FORCE_NEW_DEPLOY
-
 totalFile=$(find "$DIR_BLOG" -maxdepth 1 -type f|wc -l)
+
+echo $IS_LOCAL
+
 echo "Total file: $totalFile"
-if [[ $totalFile -gt  0 || $FORCE_NEW_DEPLOY ]]; then
+if ($IS_LOCAL); then
+    yarn dev
+elif [[ $totalFile -gt  0 || $FORCE_NEW_DEPLOY ]]; then
     ./build.sh $DOMAIN
     rm -rf $DIR_BLOG
 fi
--- src/utils/FileUtils.ts
+++ src/utils/FileUtils.ts
@@ -17,54 +17,29 @@ const IMAGES = [
     "blog-img2.webp",
     "blog-img3.webp",
     "blog-img4.webp",
-]
+];
 export const readContentFile = async (fileName: string) => {
     const directoryAppInfos = path.join(
         process.cwd(),
         "src/data/" + fileName + ".json"
     );
     let object = JSON.parse(fs.readFileSync(directoryAppInfos, "utf8"));
+    let arr = object.post_content.split("\n");
+    let newContent;
+    if (arr?.length) {
+        newContent = "<p>" + arr.join("</p><p>") + "</p>";
+    } else {
+        newContent = object.post_content;
+    }
     let post = new Post({
         title: object.post_title,
-        content: object.post_content,
+        content: newContent,
         date: object.post_date,
         slug: object.post_name,
-        image: object.post_image ? object.post_image : `images/${IMAGES[object.ID % IMAGES.length]}`
+        image: object.post_image
+            ? object.post_image
+            : `images/${IMAGES[object.ID % IMAGES.length]}`,
     });
-
-    // const directoryAppInfos = path.join(
-    //     process.cwd(),
-    //     "src/data/" + fileName + ".md"
-    // );
-    // let content = fs.readFileSync(directoryAppInfos, "utf8");
-    // // console.log("content ", content);
-
-    // let contentHTML = content;
-    // let description = "";
-    // if (content.includes(TEXT_SPLIT)) {
-    //     let arr = content.split(TEXT_SPLIT);
-    //     let infoArr = arr[1].split("\n");
-    //     for (let i = 0; i < infoArr.length; i++) {
-    //         if (infoArr[i].startsWith("description")) {
-    //             description = infoArr[i]
-    //                 .trim()
-    //                 .replace("description", "")
-    //                 .replace(': "', "");
-    //             description = description.substring(0, description.length - 1);
-    //         }
-    //     }
-    //     contentHTML = arr[2];
-    //     contentHTML = markdown.toHTML(contentHTML);
-    // }
-    // // console.log("description ", description);
-    // // console.log("contentHTML ", contentHTML);
-    // let post = new Post({
-    //     title: "THis is title",
-    //     content: contentHTML,
-    //     description: "This is description",
-    //     date: new Date(),
-    //     slug: fileName,
-    // });
     return post;
 };
 
