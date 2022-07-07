1deba4dcc41565ae06da5a31bdfcf14e426aa8e5
ManhHoangbk <manhhoangbk2@gmail.com>
2022-07-07 13:47:03 +0700

    fixDeploy

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,9 @@ mkdir $DIR_BLOG
 echo  $DOMAIN
 node node/index.js $DOMAIN $FORCE_NEW_DEPLOY
 
-if [ -d "$DIR_BLOG" ]; then
-    totalFile=$(find "$DIR_BLOG" -maxdepth 1 -type f|wc -l)
-    echo "Total file: $totalFile"
-    if [[ $totalFile -gt  0 ]]; then
-        ./build.sh $DOMAIN
-        rm -rf $DIR_BLOG
-    fi
+totalFile=$(find "$DIR_BLOG" -maxdepth 1 -type f|wc -l)
+echo "Total file: $totalFile"
+if [[ $totalFile -gt  0 || $FORCE_NEW_DEPLOY ]]; then
+    ./build.sh $DOMAIN
+    rm -rf $DIR_BLOG
 fi
