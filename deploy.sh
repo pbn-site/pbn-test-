08efd13fe8fcfb7e9619fb5f310146a0dbb79d17
ManhHoangbk <manhhoangbk2@gmail.com>
2022-07-07 21:49:56 +0700

    buildFIle

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,12 @@ mkdir $DIR_BLOG
 echo  $DOMAIN
 node node/index.js $DOMAIN $FORCE_NEW_DEPLOY
 totalFile=$(find "$DIR_BLOG" -maxdepth 1 -type f|wc -l)
-
+. $ENV
+echo 1
 echo $IS_LOCAL
-
+echo 2
 echo "Total file: $totalFile"
-if ($IS_LOCAL); then
+if ($IS_LOCAL gt 'true'); then
     yarn dev
 elif [[ $totalFile -gt  0 || $FORCE_NEW_DEPLOY ]]; then
     ./build.sh $DOMAIN
